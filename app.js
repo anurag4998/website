5f13a5ec597295adbad4c41d9cf94c82af9ee563
anurag4998 <anuragagrawal4998@gmail.com>
2020-04-18 20:38:29 +0530

    cp

--- app.js
+++ app.js
@@ -2,21 +2,36 @@ const database = firebase.database();
 const rootRef = database.ref()
 
 const preObject = document.getElementById('object');
-var button1 = document.createElement('button');
-var button2 = document.createElement('button');
-var input = document.createElement('text')
-var subtotal = document.createElement('text')
 
 
 
+const content1 = document.getElementById("cartitem")
 
-rootRef.orderByKey().on('value', snapshot => 
+
+               
+      rootRef.orderByKey().on('value', snapshot => 
+      {
+        
+        let variable = snapshot.val();
+        if(variable.rice_basket.Value == 1)  
+        {
+          checkrice()
+        }
+        
+        //////////////////////////////////////////////////////////////////////////
+        if(variable.sugar_basket.Value == 1)
+        {
+            checksugar()  
+            
+        }       
+  
+        });
+ 
+
+/////////////////////////////////////////////////////////////////////////////////////
+function checkrice()
 {
-
-  let variable = snapshot.val();
-  if(variable.rice_basket.Value == 1)  
-   {
-        var div = document.createElement('div');
+  var div = document.createElement('div');
         div.setAttribute('id', "cartitem");
         const content = document.getElementById("cart")
         content.appendChild(div) 
@@ -29,10 +44,10 @@ rootRef.orderByKey().on('value', snapshot =>
         
         var li = document.createElement('li');
         const content2 = document.getElementById("cartrow");
-        li.setAttribute('id', "productname");
+        li.style.width = '412px'
         li.textContent = 'Daawat Rozana Super Basmati Rice';
         content2.appendChild(li);
-        document.getElementById("productname").style.width = '412px';
+       
 
         var li = document.createElement('li');
 
@@ -42,9 +57,13 @@ rootRef.orderByKey().on('value', snapshot =>
 
         var li = document.createElement('li');
         li.setAttribute('id', "quantitybutton");
-        button1.setAttribute('id', "increment");
+        var button1 = document.createElement('button');
+        var input = document.createElement('text')
+        var button2 = document.createElement('button');
+      
+        
         input.setAttribute('id', "input")
-        button2.setAttribute('id', "increment");
+        
 
 
         button1.innerText = '+'
@@ -56,69 +75,144 @@ rootRef.orderByKey().on('value', snapshot =>
 
 
         var li = document.createElement('li');
+        var subtotal = document.createElement('text')
+
         content2.appendChild(li).appendChild(subtotal)
         subtotal.setAttribute('id', "subtotal");
 
         initialcartvalue()
+        button1.addEventListener('click',incrementvalue)
+        button2.addEventListener('click',decrementvalue)
+        
+        
+        
+        function initialcartvalue()
+        {
+          document.getElementById("subtotal").innerText = parseInt(document.getElementById("input").innerText) * parseInt(document.getElementById("unitprice").innerText)
+        }
+        function incrementvalue(event)
+        {
+          var button = event.target
+          var value = parseInt(document.getElementById("input").innerText)
+          value = value + 1
+          document.getElementById("input").innerText = value
+          total()
+        }
+        
+        function decrementvalue(event)
+        {
+          var button = event.target
+          var value = parseInt(document.getElementById("input").innerText)
+          if(value==0)
+          {
+            document.getElementById("input").innerText = 0
+        
+          }
+          else
+          {
+            value = value-1;
+            document.getElementById("input").innerText = value
+          }
+          total()
+        }
+        
+        function total()
+        {
+          var product = parseInt(document.getElementById("input").innerText) * parseInt(document.getElementById("unitprice").innerText)
+          document.getElementById("subtotal").innerText = product
+          a = product;
+        }
+        
+        
 
+}
+///////////////////////////////////////////////////////////////////////////////////
 
-  }
-  
-  
-  if(variable.sugar_basket.Value == 1)
-  {
-     var div = document.createElement('div');
-     div.setAttribute('id', "cartitem");
-     const content = document.getElementById("cart")
-     content.appendChild(div) 
+function checksugar()
+{
+  const content = document.getElementById("cart")
+    var div = document.createElement('div');
+    div.setAttribute('id', "cartitem");
+    content.appendChild(div)
+
+    var ul = document.createElement('ul');
+
+    content.appendChild(div).appendChild(ul)
+
+    var li =  document.createElement('li');
+    li.innerText = 'Parry Brown Sugar'
+    li.style.width = '412px'
+    content.appendChild(div).appendChild(ul).appendChild(li)
+
+    var li =  document.createElement('li');
+    li.setAttribute('id', "unitprice1")
+    li.innerText = '100';
+    content.appendChild(div).appendChild(ul).appendChild(li)
+
+    var button1 = document.createElement('button');
+    var input = document.createElement('text')
+    var button2 = document.createElement('button');
+    var li = document.createElement('li');
     
-     var ul = document.createElement('ul');
-     ul.setAttribute('id', "cartrow");
-      
-     
+    input.setAttribute('id', "input1")
 
 
-  }
-  
-})  
+        button1.innerText = '+'
+        button2.innerText = '-'
+        input.innerText = '1'
 
-button1.addEventListener('click',incrementvalue)
-button2.addEventListener('click',decrementvalue)
+        content.appendChild(div).appendChild(ul).appendChild(li).appendChild(button1)
+        content.appendChild(div).appendChild(ul).appendChild(li).appendChild(input)
+        content.appendChild(div).appendChild(ul).appendChild(li).appendChild(button2)
 
-function initialcartvalue()
-{
-  document.getElementById("subtotal").innerText = parseInt(document.getElementById("input").innerText) * parseInt(document.getElementById("unitprice").innerText)
+        var li = document.createElement('li');
+        var subtotal = document.createElement('text')
+
+        content.appendChild(div).appendChild(ul).appendChild(li).appendChild(subtotal)
+        subtotal.setAttribute('id', "subtotal1");
+
+        initialcartvalue()
+        button1.addEventListener('click',incrementvalue)
+        button2.addEventListener('click',decrementvalue)
+        
+        
+        
+        function initialcartvalue()
+        {
+          document.getElementById("subtotal1").innerText = parseInt(document.getElementById("input1").innerText) * parseInt(document.getElementById("unitprice1").innerText)
+        }
+        function incrementvalue(event)
+        {
+          var button = event.target
+          var value = parseInt(document.getElementById("input1").innerText)
+          value = value + 1
+          document.getElementById("input1").innerText = value
+          total()
+        }
+        
+        function decrementvalue(event)
+        {
+          var button = event.target
+          var value = parseInt(document.getElementById("input1").innerText)
+          if(value==0)
+          {
+            document.getElementById("input1").innerText = 0
+        
+          }
+          else
+          {
+            value = value-1;
+            document.getElementById("input1").innerText = value
+          }
+          total()
+        }
+        
+        function total()
+        {
+          var product = parseInt(document.getElementById("input1").innerText) * parseInt(document.getElementById("unitprice1").innerText)
+          document.getElementById("subtotal1").innerText = product
+          b = product;
+        }
+        
+        
 }
-function incrementvalue(event)
-{
-  var button = event.target
-  var value = parseInt(document.getElementById("input").innerText)
-  value = value + 1
-  document.getElementById("input").innerText = value
-  total()
-}
-
-function decrementvalue(event)
-{
-  var button = event.target
-  var value = parseInt(document.getElementById("input").innerText)
-  if(value==0)
-  {
-    document.getElementById("input").innerText = 0
-
-  }
-  else
-  {
-    value = value-1;
-    document.getElementById("input").innerText = value
-  }
-  total()
-}
-
-function total()
-{
-  var product = parseInt(document.getElementById("input").innerText) * parseInt(document.getElementById("unitprice").innerText)
-  document.getElementById("subtotal").innerText = product
-
-}
-
